ecd269e1a7ea3d47dfacacd2c62377a4290150ef
Pete Fowler <fowler.pete@gmail.com>
2022-06-20 19:21:26 -0600

    Add htmlwebpackplugin with template
    Old dist/index had disappeared...

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -7,8 +8,14 @@ module.exports = {
     devServer: {
         static: './dist',
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Todo List',
+            template: 'src/index.html',
+        }),
+    ],
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
