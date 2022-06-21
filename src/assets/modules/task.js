34dfb3a09829a8694cc77bb9e785e5454111fd09
Pete Fowler <fowler.pete@gmail.com>
2022-06-20 20:51:16 -0600

    Draft task module, newTask factory
    Not working. Need to properly write and import/export.

---
+++ src/assets/modules/task.js
@@ -0,0 +1,5 @@
+export {newTask}; 
+ 
+const newTask = (title, due, description, priority) => {
+        return {title, due, description, priority};
+    }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
+import {newTask} from './assets/modules/task.js';
 import './style.css';
+
+let a = newTask('run', 'running', 'march', 'high');
