6663c5a7aeba7299c321faf860b91dd4acca8a95
Pete Fowler <fowler.pete@gmail.com>
2022-06-21 13:00:31 -0600

    Draft task UI element
    Need custom style for star?

--- src/assets/modules/dom.js
+++ src/assets/modules/dom.js
@@ -18,23 +18,38 @@ function displayTask() {
     task.classList.add('task');
 
     // Input elements
+
     const title = document.createElement('input');
-    title.id = 'name';
-    title.setAttribute('placeholder', 'Name');
+    title.id = 'title';
     title.setAttribute('type', 'text');
+    title.setAttribute('placeholder', 'Title');
     title.setAttribute('minlength', '1');
     title.setAttribute('maxlength', '25');
     task.appendChild(title);
 
     const description = document.createElement('input');
     description.id = 'description';
-    
-    const priority = document.createElement('input');
-    priority.id = 'priority';
-    
+    description.setAttribute('type', 'text');
+    description.setAttribute('placeholder', 'Description');
+    description.setAttribute('minlength', '1');
+    description.setAttribute('maxlength', '74');
+    task.appendChild(description);
+
+    const starredL = document.createElement('label');
+    starredL.setAttribute('for', 'starred');
+    starredL.textContent = 'Important:';
+
+    const starred = document.createElement('input');
+    starred.id = 'starred';
+    starred.setAttribute('type', 'checkbox');
+    starredL.appendChild(starred);
+    task.appendChild(starredL);
+
     const due = document.createElement('input');
     due.id = 'due';
-
+    due.setAttribute('type', 'date');
+    due.setAttribute('placeholder', 'Name');
+    task.appendChild(due);
 
     main.insertBefore(task, newTaskBtn);
 }
