5b707b67a137e94a0343667a4aeffb6d22b12720
Pete Fowler <fowler.pete@gmail.com>
2022-06-23 15:35:35 -0600

    Add dropdown modal to screen from button click
    Need functionality

--- src/assets/modules/tasksDom.js
+++ src/assets/modules/tasksDom.js
@@ -15,62 +15,66 @@ function render() {
 
     for(let i=0; i < taskmod.tasks.length; i++) {
         const task = document.createElement('div');
-        task.id = `${i}`;
-        task.classList.add('task');
+            task.id = `${i}`;
+            task.classList.add('task');
 
         const title = document.createElement('input');
-        title.classList.add('title');
-        title.setAttribute('type', 'text');
-        title.setAttribute('placeholder', 'Title');
-        title.setAttribute('minlength', '1');
-        title.setAttribute('maxlength', '25');
-        title.value = taskmod.tasks[i].title;
-        task.appendChild(title);
+            title.classList.add('title');
+            title.setAttribute('type', 'text');
+            title.setAttribute('placeholder', 'Title');
+            title.setAttribute('minlength', '1');
+            title.setAttribute('maxlength', '25');
+            title.value = taskmod.tasks[i].title;
+            task.appendChild(title);
 
         const description = document.createElement('input');
-        description.classList.add('description');
-        description.setAttribute('type', 'text');
-        description.setAttribute('placeholder', 'Description');
-        description.setAttribute('minlength', '1');
-        description.setAttribute('maxlength', '111');
-        description.value = taskmod.tasks[i].description;
-        task.appendChild(description);
+            description.classList.add('description');
+            description.setAttribute('type', 'text');
+            description.setAttribute('placeholder', 'Description');
+            description.setAttribute('minlength', '1');
+            description.setAttribute('maxlength', '111');
+            description.value = taskmod.tasks[i].description;
+            task.appendChild(description);
 
         const starredL = document.createElement('label');
-        starredL.setAttribute('for', 'starred');
-        starredL.textContent = 'Important:';
-
-        const project = document.createElement('input');
-        project.classList = 'project';
+            starredL.setAttribute('for', 'starred');
+            starredL.textContent = 'Important:';
 
         const starred = document.createElement('input');
-        starred.classList.add('starred');
-        starred.setAttribute('type', 'checkbox');
-        starred.value = taskmod.tasks[i].starred;
-        starredL.appendChild(starred);
-        task.appendChild(starredL);
+            starred.classList.add('starred');
+            starred.setAttribute('type', 'checkbox');
+            starred.value = taskmod.tasks[i].starred;
+            starredL.appendChild(starred);
+            task.appendChild(starredL);
 
         const due = document.createElement('input');
-        due.classList.add('due');
-        due.setAttribute('type', 'date');
-        due.value = taskmod.tasks[i].due;
-        task.appendChild(due);
+            due.classList.add('due');
+            due.setAttribute('type', 'date');
+            due.value = taskmod.tasks[i].due;
+            task.appendChild(due);
 
 
         const dropDown = document.createElement('div');
-        dropDown.classList.add('dropdown');
+            dropDown.classList.add('dropdown');
 
             const dropDownBtn = document.createElement('button');
-            dropDownBtn.id = 'dropdown-button';
-            dropDownBtn.textContent = 'M';
-            dropDownBtn.addEventListener('click', expand);
+                dropDownBtn.classList.add('dropdown-button');
+                dropDownBtn.textContent = 'M';
+                dropDownBtn.addEventListener('click', handleDrop);
+                dropDown.appendChild(dropDownBtn);
 
             const dropDownContent = document.createElement('div');
-            dropDownContent.classList.add = 'dropdown-content';
-
-
-        dropDown.appendChild(dropDownBtn);
-        dropDown.appendChild(dropDownContent);
+                dropDownContent.classList.add('dropdown-content');
+                const mps = document.querySelectorAll('.menu-project');
+                mps.forEach(e => e.remove);
+                for (let i = 0; i < projects.length; i++) {
+                    let proj = document.createElement('div');
+                    proj.id = `mp${i}`;
+                    proj.classList.add('menu-project');
+                    proj.textContent = projects[i].name;
+                    dropDownContent.appendChild(proj);
+                }
+                dropDown.appendChild(dropDownContent);
 
         task.appendChild(dropDown);
 
@@ -78,10 +82,9 @@ function render() {
     }
 }
 
-function expand() {
-    document.createElement(div);
-    div.id = 'menu';
-
+function handleDrop() {
+    let ddc = document.querySelector('.dropdown-content');
+    ddc.classList.toggle('show');
 }
 
 function handleChange(e) {
